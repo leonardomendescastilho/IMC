e44cbbc54736dbed7e21e56ee6460ebcbbb7777b
Leonardo Mendes <leonardo.smendescastilho@gmail.com>
2024-05-27 18:05:18 -0300

    complete, but need some details

--- main.js
+++ main.js
@@ -1,34 +1,61 @@
-const modal = document.querySelector('#modal');
-const weightInput = document.querySelector('#weight');
-const heightInput = document.querySelector('#height');
-const button = document.querySelector('#button');
-const error = document.querySelector('.error');
 const inputs = document.querySelectorAll('.input');
-const inputSpan = document.querySelector('.span');
+
+const button = document.querySelector('#button');
 
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  // isInputNotANumber();
-  calculateIMC()
-  openModal()
+  const weight = document.querySelector('#weight').value;
+  const height = document.querySelector('#height').value;
+
+  const imcResult = calculateIMC(Number(weight), Number(height));
+
+  if (imcResult) {
+    const inputSpan = document.querySelector('.span');
+    inputSpan.innerHTML = imcResult;
+    openModal();
+  }
 });
 
-function calculateIMC() {
-  const weight = Number(weightInput.value)
-  const height = Number(heightInput.value)
-  // if (weight <= 0 || height <= 0) {
-  //   return 'Peso e altura devem ser maiores que zero';
-  // }
-  let imc =  weight / ((height / 100) ** 2);
-  document.querySelector('.span').innerHTML = imc.toFixed(2);
-}
-
-function openModal() {
-  modal.classList.add('open');
-}
-
-function isInputNotANumber() {
-  if (isNaN(Number(weight.value)) || isNaN(Number(height.value))) {
-    return error.classList.add('open');
+function calculateIMC(weight, height) {
+  try {
+    if (isNaN(weight) || isNaN(height)) {
+      modalError();
+    } else {
+      let imc = weight / (height / 100) ** 2;
+      return (imc = imc.toFixed(2));
+    }
+    throw new Error('É necessário digitar números')
+  } catch (error) {
+    console.log(error.message);
   }
+}
+function openModal() {
+  const modal = document.querySelector('#modal');
+  modal.classList.add('open');
+  const modalContainsOpen =  modal.classList.contains('open')
+
+  if(modalContainsOpen){
+    const closeIcon = document.querySelector('#close-icon');
+    closeIcon.addEventListener('click', ()=>{
+      modal.classList.remove('open');
+      // inputs.innerText = '0'; falta ver o input
+    })
+    modal.addEventListener('click', ()=>{
+      modal.classList.remove('open');
+      // inputs.innerText = '0'; falta ver o input
+    })
+  }
+}
+function modalError() {
+  const error = document.querySelector('.error');
+  error.classList.add('open');
+}
+
+
+const erroContainsOpen = error.classList.contains('open')
+
+if(erroContainsOpen){
+  inputs.addEventListener('click', ()=>{
+    error.classList.remove('open');
+  })
 }
---
+++ passos.md
@@ -0,0 +1,13 @@
+[1] Evento de Clique no botão Calcular IMC
+
+[2] Lógica: Ver se os valores digitados são números
+  [2.a] Se for letras: modal error aparece.
+  [2.b] Ao clicar em qualquer input, resetar os dois valores para o placeholder e remover o modal error.
+
+[3] Calcular o IMC com os valores  tratados (type of number)
+
+[3] Abrir segundo modal.
+
+[4] Pegar o valor do IMC calculado e substituir no span do modal
+
+[5] Ao clicar em qualquer local do modal ou no icone x fechar o modal e resetar tudo. (tentar sem carregar a página)
