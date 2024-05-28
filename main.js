07799dfc0bc0c900da9899424124314d48b6ea59
Leonardo Mendes <leonardo.smendescastilho@gmail.com>
2024-05-28 13:23:57 -0300

    complete js

--- main.js
+++ main.js
@@ -1,61 +1,71 @@
+const modal = document.querySelector('#modal');
+const span = document.querySelector('.span');
 const inputs = document.querySelectorAll('.input');
+const error = document.querySelector('.error');
+const closeIcon = document.querySelector('#close-icon')
 
+const weight = document.querySelector('#weight');
+const height = document.querySelector('#height');
 const button = document.querySelector('#button');
 
-button.addEventListener('click', (event) => {
+function isValidPositiveNumber(value) {
+  const number = Number(value);
+  const validation = number > 0 && !Number.isNaN(number);
+  return validation;
+}
+
+function startingCalculate(event) {
   event.preventDefault();
-  const weight = document.querySelector('#weight').value;
-  const height = document.querySelector('#height').value;
 
-  const imcResult = calculateIMC(Number(weight), Number(height));
+  const validWeight = isValidPositiveNumber(weight.value);
+  const validHeight = isValidPositiveNumber(height.value);
 
-  if (imcResult) {
-    const inputSpan = document.querySelector('.span');
-    inputSpan.innerHTML = imcResult;
+  if (validWeight && validHeight) {
+    const result = calculateIMC(weight.value, height.value);
+    addValueToModal(result);
     openModal();
+  } else {
+    modalError();
   }
-});
-
+}
 function calculateIMC(weight, height) {
-  try {
-    if (isNaN(weight) || isNaN(height)) {
-      modalError();
-    } else {
-      let imc = weight / (height / 100) ** 2;
-      return (imc = imc.toFixed(2));
-    }
-    throw new Error('É necessário digitar números')
-  } catch (error) {
-    console.log(error.message);
-  }
+  const imc = (weight / (height / 100) ** 2).toFixed(2);
+  return imc;
 }
+
+function addValueToModal(result) {
+  span.innerText = result;
+}
+
 function openModal() {
-  const modal = document.querySelector('#modal');
   modal.classList.add('open');
-  const modalContainsOpen =  modal.classList.contains('open')
 
-  if(modalContainsOpen){
-    const closeIcon = document.querySelector('#close-icon');
-    closeIcon.addEventListener('click', ()=>{
-      modal.classList.remove('open');
-      // inputs.innerText = '0'; falta ver o input
-    })
-    modal.addEventListener('click', ()=>{
-      modal.classList.remove('open');
-      // inputs.innerText = '0'; falta ver o input
-    })
+  const modalIsOpen = modal.classList.contains('open')
+  if(modalIsOpen){
+    closeIcon.addEventListener('click', closeModal);
+    modal.addEventListener('click', closeModal);
   }
 }
+function closeModal(){
+  modal.classList.remove('open');
+  weight.value = '';
+  height.value = '';
+  
+}
+let isOpen = false;
 function modalError() {
-  const error = document.querySelector('.error');
   error.classList.add('open');
+  isOpen = true;
+  if (isOpen) {
+    inputs.forEach((input) => {
+      input.addEventListener('click', () => {
+          weight.value = '';
+          height.value = '';
+          error.classList.remove('open');
+          isOpen = false;
+      });
+    });
+  }
 }
 
-
-const erroContainsOpen = error.classList.contains('open')
-
-if(erroContainsOpen){
-  inputs.addEventListener('click', ()=>{
-    error.classList.remove('open');
-  })
-}
+button.addEventListener('click', startingCalculate);
